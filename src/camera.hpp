bce1e233bf465cf242801a3bff061d92f81234c7
Kwasniok <Kwasniok@users.noreply.github.com>
2022-04-10 18:45:20 +0200

    add camera

---
+++ src/camera.hpp
@@ -0,0 +1,24 @@
+#ifndef CPP_RAYTRACING_CAMERA_H
+#define CPP_RAYTRACING_CAMERA_H
+
+#include "ray.hpp"
+#include "vec3.hpp"
+
+namespace ray {
+
+struct Camera {
+    long canvas_width = 0;
+    long canvas_height = 0;
+    Vec3 origin{};
+    Vec3 direction_x{};
+    Vec3 direction_y{};
+    Vec3 direction_z{};
+
+    Ray ray_for_coords(const Scalar x, const Scalar y) const {
+        return Ray(origin, direction_z + x * direction_x + y * direction_y);
+    }
+};
+
+} // namespace ray
+
+#endif
