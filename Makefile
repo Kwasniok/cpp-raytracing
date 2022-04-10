3f874c43cc1b9e7843a2e9bfab0ec7b51e0a6cb3
Kwasniok <Kwasniok@users.noreply.github.com>
2022-04-10 16:15:07 +0200

    add ppm example

--- Makefile
+++ Makefile
@@ -1,12 +1,16 @@
 
 .PHONY: all
 .DEFAULT_GOAL=all
-all: folders main
+all: folders main run
 
 # ensure folders exist
 .PHONY: folders
 folders:
 	mkdir -p src bin out
+
+.PHONY:run
+run:
+	./bin/main > out/out.ppm
 
 .PHONY: main
 main: bin/main
--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,28 @@
-int main(void) {}
+#include <iostream>
+
+using namespace std;
+
+void print_example_ppm_file() {
+    // see: https://en.wikipedia.org/wiki/Netpbm#File_formats
+
+    constexpr unsigned long width = 100;
+    constexpr unsigned long height = 100;
+
+    // header
+    cout << "P1 # bitmap" << endl;
+    cout << width << " " << height << " # width x height" << endl;
+
+    // content
+    for (unsigned long i = 0; i < width; ++i) {
+        for (unsigned long j = 0; j < height; ++j) {
+            if ((i + j) % 2 == 0) {
+                cout << " 0";
+            } else {
+                cout << " 1";
+            }
+        }
+        cout << endl;
+    }
+}
+
+int main(int argc, char** argv) { print_example_ppm_file(); }
