9a745415a00f769069d4b12b72977a6dfef304ff
Kwasniok <Kwasniok@users.noreply.github.com>
2022-04-10 19:25:10 +0200

    add colors

--- src/color.hpp
+++ src/color.hpp
@@ -96,6 +96,7 @@ inline constexpr Color operator/(const Color& color, const ColorScalar f) {
 
 namespace Colors {
 constexpr Color BLACK{0.0, 0.0, 0.0};
+constexpr Color WHITE{1.0, 1.0, 1.0};
 constexpr Color RED{1.0, 0.0, 0.0};
 constexpr Color GREEN{0.0, 1.0, 0.0};
 constexpr Color BLUE{0.0, 0.0, 1.0};
