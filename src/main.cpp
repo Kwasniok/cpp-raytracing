f4118bef0e4e7533d78029e1f492f790e7f8eb6c
Kwasniok <Kwasniok@users.noreply.github.com>
2022-04-10 20:19:00 +0200

    show normal

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,11 @@
 using namespace std;
 using namespace ray;
 
-bool constexpr hits(const Ray& ray, const Sphere& sphere) {
+Scalar constexpr hits_at(const Ray& ray, const Sphere& sphere) {
+    // returns minimal solution which is positive and starts outside of the
+    // sphere or else nan
+    constexpr Scalar nan = numeric_limits<Scalar>::signaling_NaN();
+
     // analytical geometry: line hits sphere
     // ray: s + t*d
     // sphere: (x-o)^2 = R^2
@@ -23,10 +27,20 @@ bool constexpr hits(const Ray& ray, const Sphere& sphere) {
     const auto discriminant = b * b - 4.0 * a * c;
     if (discriminant < 0.0) {
         // no real solution
-        return false;
+        return nan;
     }
-    const auto t = (-b - sqrt(discriminant)) / (2.0 * a);
-    return t > 0.0;
+    const auto t1 = (-b - sqrt(discriminant)) / (2.0 * a);
+    // const auto t2 = (-b + sqrt(discriminant)) / (2.0 * a);
+    // t1>0, t2>0: ray starts outside of the sphere and enters it at t1
+    // t1<0, t2>0: ray starts inside the sphere and leaves it at t2
+    // t1<0, t2<0: ray starts after the sphere and never enters it
+
+    // select minimal positive sloution (if it exists)
+    if (t1 > 0.0) {
+        return t1;
+    }
+    // no positive soltion which is minimal
+    return nan;
 }
 
 Color constexpr ray_back_ground_color(const Ray& ray) {
@@ -38,9 +52,12 @@ Color constexpr ray_back_ground_color(const Ray& ray) {
 }
 
 Color constexpr ray_color(const Ray& ray) {
-    constexpr Sphere sphere{{0.0, 0.0, 1.0}, 0.5};
-    if (hits(ray, sphere)) {
-        return Colors::RED;
+    constexpr Sphere sphere{{0.0, 0.0, -1.0}, 0.5};
+    const auto t = hits_at(ray, sphere);
+    if (t > 0.0) {
+        const auto hit_point = ray.at(t);
+        const auto normal = unit_vector(hit_point - sphere.origin());
+        return 0.5 * Color(normal.x() + 1, normal.y() + 1, normal.z() + 1);
     }
     return ray_back_ground_color(ray);
 }
@@ -54,7 +71,7 @@ void print_example_ppm_file() {
                         .origin = {0.0, 0.0, 0.0},
                         .direction_x = {1.0, 0.0, 0.0},
                         .direction_y = {0.0, 1.0, 0.0},
-                        .direction_z = {0.0, 0.0, 1.0}};
+                        .direction_z = {0.0, 0.0, -1.0}};
 
     // header
     cout << "P3 # ASCII RGB" << endl;
@@ -64,7 +81,7 @@ void print_example_ppm_file() {
 
     // content (checker board)
     cerr << "Rendering image ..." << endl;
-    for (long j = 0; j < camera.canvas_height; ++j) {
+    for (long j = camera.canvas_height - 1; j >= 0; --j) {
         cerr << "line " << (j + 1) << "/" << camera.canvas_height << endl;
         for (long i = 0; i < camera.canvas_width; ++i) {
             const Scalar x = 2.0 * (Scalar(i) / camera.canvas_width - 0.5);
