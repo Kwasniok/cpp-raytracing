bf98c9b5f8b0161a1ed37b96ad44dfbca569e3bb
Kwasniok <Kwasniok@users.noreply.github.com>
2022-04-11 18:05:23 +0200

    add logging flag

--- src/main.cpp
+++ src/main.cpp
@@ -64,10 +64,13 @@ Color constexpr ray_color(const Ray& ray) {
 
 void print_example_ppm_file() {
     // see: https://en.wikipedia.org/wiki/Netpbm#File_formats
+
+    const bool logging = false;
+
     const unsigned long max_color = 255;
 
-    const Camera camera{.canvas_width = 100,
-                        .canvas_height = 100,
+    const Camera camera{.canvas_width = 1000,
+                        .canvas_height = 1000,
                         .origin = {0.0, 0.0, 0.0},
                         .direction_x = {1.0, 0.0, 0.0},
                         .direction_y = {0.0, 1.0, 0.0},
@@ -80,9 +83,13 @@ void print_example_ppm_file() {
     cout << max_color << " # max color value per channel" << endl;
 
     // content (checker board)
-    cerr << "Rendering image ..." << endl;
+    if (logging) {
+        cerr << "Rendering image ..." << endl;
+    }
     for (long j = camera.canvas_height - 1; j >= 0; --j) {
-        cerr << "line " << (j + 1) << "/" << camera.canvas_height << endl;
+        if (logging) {
+            cerr << "line " << (j + 1) << "/" << camera.canvas_height << endl;
+        }
         for (long i = 0; i < camera.canvas_width; ++i) {
             const Scalar x = 2.0 * (Scalar(i) / camera.canvas_width - 0.5);
             const Scalar y = 2.0 * (Scalar(j) / camera.canvas_height - 0.5);
