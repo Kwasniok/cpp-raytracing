a01070941d5d420e9b62d05c6e404308399fbbff
Kwasniok <Kwasniok@users.noreply.github.com>
2022-04-15 20:05:02 +0200

    extend util

--- src/util.hpp
+++ src/util.hpp
@@ -19,6 +19,17 @@ template <typename T, T low, T high> inline constexpr T clip(const T x) {
     return x;
 }
 
+template <typename T>
+inline constexpr T clip(const T x, const T low, const T high) {
+    if (x < low) {
+        return low;
+    }
+    if (x > high) {
+        return high;
+    }
+    return x;
+}
+
 } // namespace ray
 
 #endif
