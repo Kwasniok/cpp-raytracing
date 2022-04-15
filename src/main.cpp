af99343b9b69c83eea646ccf7283359195950fdd
Kwasniok <Kwasniok@users.noreply.github.com>
2022-04-15 20:09:34 +0200

    update demo scene

--- src/main.cpp
+++ src/main.cpp
@@ -15,10 +15,26 @@ using namespace std;
 using namespace ray;
 
 void make_scene(Scene& scene) {
-    std::shared_ptr<Material> material =
+    // diffuse
+    std::shared_ptr<Material> diffuse_gray =
         make_shared<Material>(Material{.diffuse_color = {0.5, 0.5, 0.5}});
-    scene.add(make_unique<Sphere>(Vec3(0.0, 0.0, -1.0), 0.5, material));
-    scene.add(make_unique<Sphere>(Vec3(0.0, -100.5, -1.0), 100.0, material));
+    std::shared_ptr<Material> diffuse_red =
+        make_shared<Material>(Material{.diffuse_color = {0.75, 0.5, 0.5}});
+    std::shared_ptr<Material> diffuse_green =
+        make_shared<Material>(Material{.diffuse_color = {0.5, 0.75, 0.5}});
+    // metal
+    std::shared_ptr<Material> metal = make_shared<Material>(
+        Material{.reflection_color = {0.8, 0.7, 0.6}, .reflection = 1.0});
+
+    // left
+    scene.add(make_unique<Sphere>(Vec3(-1.0, 0.0, -1.0), 0.5, diffuse_red));
+    // middle
+    scene.add(make_unique<Sphere>(Vec3(0.0, 0.0, -1.0), 0.5, metal));
+    // right
+    scene.add(make_unique<Sphere>(Vec3(+1.0, 0.0, -1.0), 0.5, diffuse_green));
+    // floor
+    scene.add(
+        make_unique<Sphere>(Vec3(0.0, -100.5, -1.0), 100.0, diffuse_gray));
 }
 
 void print_example_ppm_file() {
@@ -30,10 +46,10 @@ void print_example_ppm_file() {
     const unsigned long ray_depth = 50;
     const unsigned long ray_depth_reflection = 10;
 
-    const Camera camera{.canvas_width = 250,
+    const Camera camera{.canvas_width = 500,
                         .canvas_height = 250,
                         .origin = {0.0, 0.0, 0.0},
-                        .direction_x = {1.0, 0.0, 0.0},
+                        .direction_x = {2.0, 0.0, 0.0},
                         .direction_y = {0.0, 1.0, 0.0},
                         .direction_z = {0.0, 0.0, -1.0}};
 
