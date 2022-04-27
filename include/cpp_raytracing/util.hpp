3f9d7e989fbb738e200df7fad3a608b07ef6d356
Kwasniok <Kwasniok@users.noreply.github.com>
2022-04-27 15:13:47 +0200

    add enumerate

--- include/cpp_raytracing/util.hpp
+++ include/cpp_raytracing/util.hpp
@@ -7,6 +7,7 @@
 #define CPP_RAYTRACING_UTIL_HPP
 
 #include <numbers>
+#include <tuple>
 
 namespace cpp_raytracing {
 
@@ -36,6 +37,39 @@ inline constexpr T clip(const T x, const T low, const T high) {
         return high;
     }
     return x;
+}
+
+/**
+ * @brief iterate over a container wiht counter
+ * @note example: @code
+ * std::array<int, 3> list= {1, 2, 3};
+ * for (auto [i, elem] : enumerate(list))
+ * {
+ *     // i is the index and elem is the i-th element
+ * }
+ * @endcode
+ */
+template <typename T, typename TIter = decltype(std::begin(std::declval<T>())),
+          typename = decltype(std::end(std::declval<T>()))>
+constexpr auto enumerate(T&& iterable) {
+    struct iterator {
+        size_t i;
+        TIter iter;
+        bool operator!=(const iterator& other) const {
+            return iter != other.iter;
+        }
+        void operator++() {
+            ++i;
+            ++iter;
+        }
+        auto operator*() const { return std::tie(i, *iter); }
+    };
+    struct iterable_wrapper {
+        T iterable;
+        auto begin() { return iterator{0, std::begin(iterable)}; }
+        auto end() { return iterator{0, std::end(iterable)}; }
+    };
+    return iterable_wrapper{std::forward<T>(iterable)};
 }
 
 } // namespace cpp_raytracing
