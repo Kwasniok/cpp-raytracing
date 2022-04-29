cff491ac614b83fc6342c346996fac687dd24fcf
Kwasniok <Kwasniok@users.noreply.github.com>
2022-04-29 16:10:36 +0200

    idea for generalized material unittest

--- src/test/cpp_raytracing/material.cpp
+++ src/test/cpp_raytracing/material.cpp
@@ -1,5 +1,7 @@
 #include <cmath>
+#include <map>
 #include <memory>
+#include <tuple>
 
 #include <cpp_raytracing/material.hpp>
 
@@ -8,6 +10,186 @@
 namespace cpp_raytracing { namespace test {
 
 constexpr Scalar epsilon = 1.0e-12;
+
+using SphereCoords = std::tuple<Scalar, Scalar>;
+
+constexpr Vec3 unit_vector_from_coords(const Scalar theta, const Scalar phi) {
+    return {
+        std::sin(theta) * std::cos(phi),
+        std::sin(theta) * std::sin(phi),
+        std::cos(theta),
+    };
+}
+
+constexpr SphereCoords coords_from_unit_vector(const Vec3 vec) {
+    return {
+        std::atan2(vec.y(), vec.x()),
+        std::acos(vec.z()),
+    };
+}
+
+using SphereBin = std::tuple<int, int>;
+
+struct StatisticsConfiguration {
+    int theta_bins = 3;
+    int phi_bins = 3;
+    Scalar theta_min = 0.0;
+    Scalar theta_max = SCALAR_PI;
+    Scalar phi_min = 0.0;
+    Scalar phi_max = 2 * SCALAR_PI;
+    int repititions = 10;
+};
+
+template <typename T>
+struct BinnedSphereMap {
+
+    const StatisticsConfiguration& config;
+    std::vector<T> data;
+
+    BinnedSphereMap(const StatisticsConfiguration& config,
+                    const T& initial = T()) // Note: this fails!
+        : config(config), data(config.theta_bins * config.phi_bins, initial) {}
+    BinnedSphereMap(BinnedSphereMap&&) = default;
+    BinnedSphereMap(const BinnedSphereMap&) = delete;
+
+    T& operator[](const SphereBin bin) {
+        auto [theta, phi] = bin;
+        return data[theta * config.phi_bins + phi];
+    }
+};
+
+struct RayStatistics {
+
+    const StatisticsConfiguration& config;
+    BinnedSphereMap<int> heatmap;
+    BinnedSphereMap<Color> total_color;
+    int zero_vector_heat = 0;
+    Color zero_vector_total_color{};
+
+    RayStatistics(const StatisticsConfiguration& config)
+        : config(config), heatmap(config), total_color(config) {}
+    RayStatistics(RayStatistics&&) = default;
+    RayStatistics(const RayStatistics&) = delete;
+
+    int theta_bin(const Scalar theta) const {
+        int bin = ((Scalar(theta) - config.theta_min) * config.theta_bins /
+                   (config.theta_max - config.theta_min));
+        if (bin == config.theta_bins) {
+            return config.theta_bins - 1;
+        }
+        return bin;
+    }
+
+    int phi_bin(const Scalar phi) const {
+        int bin = ((Scalar(phi) - config.phi_min) * config.phi_bins /
+                   (config.phi_max - config.phi_min));
+        if (bin == config.phi_bins) {
+            return config.phi_bins - 1;
+        }
+        return bin;
+    }
+
+    void register_ray(const Ray& ray, const Color& color) {
+        if (ray.direction().near_zero(epsilon)) {
+            zero_vector_heat += 1;
+            zero_vector_total_color += color;
+        } else {
+            const auto direction = unit_vector(ray.direction());
+            const auto [theta, phi] = coords_from_unit_vector(direction);
+            const auto theta_i = theta_bin(theta);
+            const auto phi_i = theta_bin(phi);
+            heatmap[{theta_i, phi_i}] += 1;
+            total_color[{theta_i, phi_i}] += color;
+        }
+    }
+
+    Color avg_color(const SphereBin bin) {
+        if (heatmap[bin] > 0) {
+            return total_color[bin] / heatmap[bin];
+        } else {
+            return Colors::BLACK;
+        }
+    }
+
+    Color zero_vector_avg_color() {
+        if (zero_vector_heat > 0) {
+            return zero_vector_total_color / zero_vector_heat;
+        } else {
+            return Colors::BLACK;
+        }
+    }
+};
+
+struct Statistics {
+    const StatisticsConfiguration& config;
+    std::shared_ptr<Material> material;
+    BinnedSphereMap<RayStatistics> ray_stats;
+
+    Statistics(const StatisticsConfiguration& config,
+               std::shared_ptr<Material> material)
+        : config(config),
+          material(material),
+          ray_stats(config, RayStatistics(config)) {}
+    Statistics(Statistics&&) = default;
+    Statistics(const Statistics&) = delete;
+
+    RayStatistics collect_ray_statistics_for_hit(const Ray& ray_in,
+                                                 const HitRecord& record) {
+
+        RayStatistics stats{config};
+        for (int i = 0; i < config.repititions; ++i) {
+            auto [ray_out, color] = material->scatter(record, ray_in);
+            TEST_ASSERT_ALMOST_EQUAL_ITERABLE(ray_out.start(), record.point,
+                                              epsilon);
+            stats.register_ray(ray_out, color);
+        }
+        return stats;
+    }
+
+    void collect_ray_statistics() {
+
+        for (Scalar theta_i = 0; theta_i < config.theta_bins; ++theta_i) {
+            const Scalar theta =
+                theta_i * (config.theta_max / config.theta_bins);
+
+            for (Scalar phi_i = 0; phi_i < config.phi_bins; ++phi_i) {
+                const Scalar phi = phi_i * (config.phi_max / config.phi_bins);
+
+                // ray from unit sphere hits segment of x-y plane in origin
+                const Vec3 hit_point = {0.0, 0.0, 0.0};
+                const Ray ray_in{
+                    unit_vector_from_coords(theta, phi),
+                    -unit_vector_from_coords(theta, phi),
+                };
+                const HitRecord record{
+                    .point = hit_point,
+                    .normal = {0.0, 0.0, 1.0},
+                    .material = material,
+                    .t = 1.0,
+                    .front_face = true,
+                };
+                ray_stats[{theta_i, phi_i}] =
+                    collect_ray_statistics_for_hit(ray_in, record);
+            }
+        }
+    }
+};
+
+void test_example() {
+    const StatisticsConfiguration config{
+        .theta_bins = 18,
+        .phi_bins = 36,
+        .theta_min = 0.0,
+        .theta_max = SCALAR_PI,
+        .phi_min = 0.0,
+        .phi_max = 2 * SCALAR_PI,
+        .repititions = 10,
+    };
+
+    Color color{0.0, 0.5, 1.0};
+    std::shared_ptr<Material> material = std::make_shared<Emitter>(color);
+    Statistics stats{config, material};
+}
 
 void test_emitter() {
     /*
@@ -192,8 +374,8 @@ void test_dielectric_air() {
              incoming ray
      outgoing ray (reflection)
 
-     outcome: ray is probabilistic reflected or refracted based on the angle and
-              IOR
+     outcome: ray is probabilistic reflected or refracted based on the angle
+     and IOR
      */
     const Color mat_col{0.0, 0.5, 1.0};
     const Scalar ior = 1.0;
@@ -238,8 +420,8 @@ void test_dielectric_into_glass() {
         v    incoming ray
      outgoing ray (reflection)
 
-     outcome: ray is probabilistic reflected or refracted based on the angle and
-              IOR
+     outcome: ray is probabilistic reflected or refracted based on the angle
+     and IOR
      */
     const Color mat_col{0.0, 0.5, 1.0};
     const Scalar ior = 1.5;
@@ -345,13 +527,13 @@ void test_dielectric_total_reflection() {
 }
 
 void run_test_suite() {
-    run(test_emitter);
-    run(test_diffuse);
-    run(test_metal_no_roughness);
-    run(test_metal_with_roughness);
-    run(test_dielectric_air);
-    run(test_dielectric_into_glass);
-    run(test_dielectric_total_reflection);
+    run(test_example);
+    // run(test_emitter);
+    // run(test_diffuse);
+    // run(test_metal_no_roughness);
+    // run(test_metal_with_roughness);
+    // run(test_dielectric_air);
+    // run(test_dielectric_into_glass);
+    // run(test_dielectric_total_reflection);
 }
-
 }} // namespace cpp_raytracing::test
