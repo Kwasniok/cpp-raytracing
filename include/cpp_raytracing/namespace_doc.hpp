a56149a1030b7f17310db43f11ed43b5450fca70
Kwasniok <Kwasniok@users.noreply.github.com>
2022-05-02 04:47:17 +0200

    update doc

--- include/cpp_raytracing/namespace_doc.hpp
+++ include/cpp_raytracing/namespace_doc.hpp
@@ -7,4 +7,11 @@
  * @brief namespace for all cpp-raytracing objects
  */
 namespace cpp_raytracing {
+
+/**
+ * @brief namespace for unittests
+ */
+namespace test {
 }
+
+} // namespace cpp_raytracing
