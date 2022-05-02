8d5cae22ecca769dc5e93f2fb2ad32b0dacf1076
Kwasniok <Kwasniok@users.noreply.github.com>
2022-05-02 18:33:22 +0200

    add tests

--- include/cpp_raytracing/io/materials.hpp
+++ include/cpp_raytracing/io/materials.hpp
@@ -6,9 +6,88 @@
 #ifndef CPP_RAYTRACING_IO_MATERIALS_HPP
 #define CPP_RAYTRACING_IO_MATERIALS_HPP
 
+#include <memory>
+#include <typeinfo>
+
+#include "../materials.hpp"
+#include "base.hpp"
 #include "materials/dielectric.hpp"
 #include "materials/diffuse.hpp"
 #include "materials/emitter.hpp"
 #include "materials/metal.hpp"
+
+namespace cpp_raytracing { namespace io {
+namespace grammar {
+
+/** @brief grammar for ::Material */
+struct material
+    : pegtl::sor<mat_emitter, mat_diffuse, mat_metal, mat_dielectric> {};
+
+/** @brief grammar for parsing a ::Material */
+template <>
+struct grammar_for<std::shared_ptr<Material>> {
+    /** @brief grammar to be selected */
+    using get = material;
+};
+
+} // namespace grammar
+
+namespace {
+
+/** @brief downcast from shared pointer and write to stream */
+template <typename D, typename B>
+inline void write_cast(std::ostream& os, const std::shared_ptr<B>& val) {
+    static_assert(std::is_base_of<B, D>::value, "D must be derived class of D");
+    write<D>(os, dynamic_cast<const D&>(*val.get()));
+}
+
+} // namespace
+
+/** @brief write a ::Material to a stream */
+template <>
+void write<std::shared_ptr<Material>>(std::ostream& os,
+                                      const std::shared_ptr<Material>& val) {
+    // NOTE: This is not the object orientated approach!
+    //       But otherwise inheritance from an abstract IO interface would be
+    //       needed for all(!) data types. In addition this would also couple
+    //       data types and io. (Currently IO depends on data types and not vice
+    //       versa.)
+    if (typeid(*val) == typeid(Emitter)) {
+        write_cast<Emitter>(os, val);
+    }
+    if (typeid(*val) == typeid(Diffuse)) {
+        write_cast<Diffuse>(os, val);
+    }
+    if (typeid(*val) == typeid(Metal)) {
+        write_cast<Metal>(os, val);
+    }
+    if (typeid(*val) == typeid(Dielectric)) {
+        write_cast<Dielectric>(os, val);
+    }
+}
+
+/**
+ *@brief parse a ::Material from a node tree
+ */
+template <>
+std::shared_ptr<Material> parse_node(const tao::pegtl::parse_tree::node& node) {
+    if (node.is_type<grammar::mat_emitter>()) {
+        return std::make_shared<Emitter>(parse_node<Emitter>(node));
+    }
+    if (node.is_type<grammar::mat_diffuse>()) {
+        return std::make_shared<Diffuse>(parse_node<Diffuse>(node));
+    }
+    if (node.is_type<grammar::mat_metal>()) {
+        return std::make_shared<Metal>(parse_node<Metal>(node));
+    }
+    if (node.is_type<grammar::mat_dielectric>()) {
+        return std::make_shared<Dielectric>(parse_node<Dielectric>(node));
+    }
+
+    // this part is never reached ...
+    throw ParsingException("Parse material implementation incomplete.");
+}
+
+}} // namespace cpp_raytracing::io
 
 #endif
---
+++ src/test/cpp_raytracing/io/materials_.cpp
@@ -0,0 +1,119 @@
+#include <cpp_raytracing/io/materials.hpp>
+#include <sstream>
+#include <utility>
+#include <vector>
+
+#include "../test.hpp"
+
+namespace cpp_raytracing { namespace test {
+
+void test_write() {
+    const std::vector<std::pair<std::shared_ptr<Material>, const char*>> data =
+        {
+            {
+                std::make_shared<Emitter>(Color{0.0, 0.5, 1}),
+                "Emitter {color = {0, 0.5, 1}}",
+            },
+            {
+                std::make_shared<Diffuse>(Color{0.0, 0.5, 1}),
+                "Diffuse {color = {0, 0.5, 1}}",
+            },
+            {
+                std::make_shared<Metal>(Color{0.0, 0.5, 1}, 0.7),
+                "Metal {color = {0, 0.5, 1}, roughness = 0.7}",
+            },
+            {
+                std::make_shared<Dielectric>(Color{0.0, 0.5, 1}, 0.7),
+                "Dielectric {color = {0, 0.5, 1}, ior = 0.7}",
+            },
+        };
+    for (const auto& [val, str] : data) {
+        std::stringstream os;
+        io::write(os, val);
+        TEST_ASSERT_EQUAL(os.str(), str);
+    }
+}
+
+void test_read_success_emitter() {
+    const std::vector<std::pair<std::shared_ptr<Emitter>, const char*>> data = {
+        {
+            std::make_shared<Emitter>(Color{0.0, 0.5, 1}),
+            "Emitter {color = {0, 0.5, 1}}",
+        },
+    };
+    for (const auto& [val, str] : data) {
+        std::shared_ptr<Material> x = io::read<std::shared_ptr<Material>>(str);
+        TEST_ASSERT_DYNAMIC_TYPE(*x.get(), Emitter);
+        Emitter* mat = dynamic_cast<Emitter*>(x.get());
+        TEST_ASSERT_EQUAL(mat->color, val->color);
+    }
+}
+
+void test_read_success_diffuse() {
+    const std::vector<std::pair<std::shared_ptr<Diffuse>, const char*>> data = {
+        {
+            std::make_shared<Diffuse>(Color{0.0, 0.5, 1}),
+            "Diffuse {color = {0, 0.5, 1}}",
+        },
+    };
+    for (const auto& [val, str] : data) {
+        std::shared_ptr<Material> x = io::read<std::shared_ptr<Material>>(str);
+        TEST_ASSERT_DYNAMIC_TYPE(*x.get(), Diffuse);
+        Diffuse* mat = dynamic_cast<Diffuse*>(x.get());
+        TEST_ASSERT_EQUAL(mat->color, val->color);
+    }
+}
+
+void test_read_success_metal() {
+    const std::vector<std::pair<std::shared_ptr<Metal>, const char*>> data = {
+        {
+            std::make_shared<Metal>(Color{0.0, 0.5, 1}, 0.7),
+            "Metal {color = {0, 0.5, 1}, roughness = 0.7}",
+        },
+    };
+    for (const auto& [val, str] : data) {
+        std::shared_ptr<Material> x = io::read<std::shared_ptr<Material>>(str);
+        TEST_ASSERT_DYNAMIC_TYPE(*x.get(), Metal);
+        Metal* mat = dynamic_cast<Metal*>(x.get());
+        TEST_ASSERT_EQUAL(mat->color, val->color);
+        TEST_ASSERT_EQUAL(mat->roughness, val->roughness);
+    }
+}
+
+void test_read_success_dielectric() {
+    const std::vector<std::pair<std::shared_ptr<Dielectric>, const char*>>
+        data = {
+            {
+                std::make_shared<Dielectric>(Color{0.0, 0.5, 1}, 0.7),
+                "Dielectric {color = {0, 0.5, 1}, ior = 0.7}",
+            },
+        };
+    for (const auto& [val, str] : data) {
+        std::shared_ptr<Material> x = io::read<std::shared_ptr<Material>>(str);
+        TEST_ASSERT_DYNAMIC_TYPE(*x.get(), Dielectric);
+        Dielectric* mat = dynamic_cast<Dielectric*>(x.get());
+        TEST_ASSERT_EQUAL(mat->color, val->color);
+        TEST_ASSERT_EQUAL(mat->index_of_refraction, val->index_of_refraction);
+    }
+}
+
+void test_read_failure() {
+    const std::vector<const char*> data = {
+        "UnsopportedMaterial {color = {0, 0.5, 1}}",
+    };
+    for (const auto& str : data) {
+        TEST_ASSERT_THROWS((io::read<std::shared_ptr<Material>>(str)),
+                           io::ParsingException);
+    }
+}
+
+void run_test_suite() {
+    run(test_write);
+    run(test_read_success_emitter);
+    run(test_read_success_diffuse);
+    run(test_read_success_metal);
+    run(test_read_success_dielectric);
+    run(test_read_failure);
+}
+
+}} // namespace cpp_raytracing::test
