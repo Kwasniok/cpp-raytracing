e7cfe8e8376bde160c6527fd530042d8648b6cc3
Kwasniok <Kwasniok@users.noreply.github.com>
2022-05-13 23:46:57 +0200

    extend test

--- src/test/cpp_raytracing/identifier.cpp
+++ src/test/cpp_raytracing/identifier.cpp
@@ -8,6 +8,11 @@
 
 namespace cpp_raytracing { namespace test {
 
+void test_default() {
+    Identifier<void> identifier{};
+    TEST_ASSERT_EQUAL(identifier.str(), default_identifier<void>::value);
+}
+
 void test_make_if_available() {
     const char* str = "good_ID1";
     auto opt_identifier = Identifier<void>::make_if_available({str});
@@ -21,6 +26,24 @@ void test_make_always() {
     const char* str{"good_ID1"};
     Identifier<void> identifier = Identifier<void>::make_always({str});
     TEST_ASSERT_EQUAL(identifier.str(), str);
+}
+
+void test_move() {
+    {
+        // move initialize
+        Identifier<void> id1;
+        const std::string str = id1.str();
+        Identifier<void> id2 = std::move(id1);
+        TEST_ASSERT_EQUAL(id2.str(), str);
+    }
+    {
+        // move assign
+        Identifier<void> id1;
+        const std::string str = id1.str();
+        Identifier<void> id2;
+        id2 = std::move(id1);
+        TEST_ASSERT_EQUAL(id2.str(), str);
+    }
 }
 
 void test_valid_good() {
@@ -61,8 +84,10 @@ void test_copy() {
 }
 
 void run_test_suite() {
+    run(test_default);
     run(test_make_if_available);
     run(test_make_always);
+    run(test_move);
     run(test_valid_good);
     run(test_valid_bad);
     run(test_comparison);
