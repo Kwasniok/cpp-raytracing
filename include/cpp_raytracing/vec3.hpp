2bcde43fcdfec1ade7aa437825e7a075c65c1dd6
Kwasniok <Kwasniok@users.noreply.github.com>
2022-05-31 17:08:22 +0200

    refactor vec3

--- include/cpp_raytracing/vec3.hpp
+++ include/cpp_raytracing/vec3.hpp
@@ -10,6 +10,9 @@
 #include <cmath>
 #include <iostream>
 
+#include <glm/detail/qualifier.hpp>
+#include <glm/geometric.hpp>
+
 #include "random.hpp"
 #include "scalar.hpp"
 
@@ -33,34 +36,34 @@ class Vec3 {
     }
 
     /** @brief get x coefficient */
-    constexpr Scalar x() const { return _data[0]; }
+    constexpr Scalar x() const { return _data.x; }
     /** @brief get y coefficient */
-    constexpr Scalar y() const { return _data[1]; }
+    constexpr Scalar y() const { return _data.y; }
     /** @brief get z coefficient */
-    constexpr Scalar z() const { return _data[2]; }
+    constexpr Scalar z() const { return _data.z; }
 
     /** @brief const iterator for first element */
-    constexpr auto begin() const { return _data.begin(); }
+    constexpr auto begin() const { return &_data[0]; }
     /** @brief const iterator for end */
-    constexpr auto end() const { return _data.end(); }
+    constexpr auto end() const { return &_data[0] + 3; }
     /** @brief iterator for first element */
-    constexpr auto begin() { return _data.begin(); }
+    constexpr auto begin() { return &_data[0]; }
     /** @brief iterator for end */
-    constexpr auto end() { return _data.end(); }
+    constexpr auto end() { return &_data[0] + 3; }
 
     /** @brief tests equivalence */
     constexpr bool operator==(const Vec3& other) const {
-        return x() == other.x() && y() == other.y() && z() == other.z();
+        return _data == other._data;
     }
 
     /** @brief tests inequivalence */
     constexpr bool operator!=(const Vec3& other) const {
-        return x() != other.x() || y() != other.y() || z() != other.z();
+        return _data != other._data;
     }
 
     /** @brief negate elementwise */
     constexpr Vec3 operator-() const {
-        return Vec3{-_data[0], -_data[1], -_data[2]};
+        return Vec3{-_data.x, -_data.y, -_data.z};
     }
 
     /**
@@ -76,42 +79,57 @@ class Vec3 {
 
     /** @brief add elementwise */
     constexpr Vec3& operator+=(const Vec3& other) {
-        _data[0] += other._data[0];
-        _data[1] += other._data[1];
-        _data[2] += other._data[2];
+        _data += other._data;
         return *this;
     }
     /** @brief subtract elementwise */
-    constexpr Vec3& operator-=(const Vec3& other) { return *this += (-other); }
+    constexpr Vec3& operator-=(const Vec3& other) {
+        _data -= other._data;
+        return *this;
+    }
     /** @brief multiply elementwise */
     constexpr Vec3& operator*=(const Scalar fac) {
-        _data[0] *= fac;
-        _data[1] *= fac;
-        _data[2] *= fac;
+        _data *= fac;
         return *this;
     }
     /** @brief divide elementwise */
-    constexpr Vec3& operator/=(const Scalar fac) { return *this *= (1 / fac); }
+    constexpr Vec3& operator/=(const Scalar fac) {
+        _data /= fac;
+        return *this;
+    }
 
     /** @brief get length of the vector */
-    constexpr Scalar length() const { return sqrt(length_squared()); }
+    constexpr Scalar length() const {
+        // NOTE: for version 0.9.9 glm::length is not constexpr
+        return sqrt(length_squared());
+    }
 
     /**
      * @brief get length of the vector squared
      * @note Fater than std::pow(length(),2)`
      */
-    constexpr Scalar length_squared() const {
-        return _data[0] * _data[0] + _data[1] * _data[1] + _data[2] * _data[2];
-    }
+    constexpr Scalar length_squared() const { return glm::dot(_data, _data); }
 
     /** @brief tests if vector is zero vector */
     constexpr bool near_zero(const Scalar epsilon) const {
-        return std::abs(_data[0]) < epsilon && std::abs(_data[1]) < epsilon &&
-               std::abs(_data[2]) < epsilon;
+        return std::abs(_data.x) < epsilon && std::abs(_data.y) < epsilon &&
+               std::abs(_data.z) < epsilon;
     }
 
+    friend inline constexpr Vec3 operator+(const Vec3& v1, const Vec3& v2);
+    friend inline constexpr Vec3 operator-(const Vec3& v1, const Vec3& v2);
+    friend inline constexpr Vec3 operator*(const Vec3& v, const Scalar f);
+    friend inline constexpr Vec3 operator*(const Scalar f, const Vec3& v);
+    friend inline constexpr Vec3 operator/(const Vec3& v, const Scalar f);
+    friend inline constexpr Scalar dot(const Vec3& v1, const Vec3& v2);
+    friend inline constexpr Vec3 cross(const Vec3& v1, const Vec3& v2);
+    friend inline constexpr Vec3 unit_vector(const Vec3& v);
+
   private:
-    std::array<Scalar, 3> _data;
+    using data_type = glm::vec<3, Scalar>;
+    constexpr Vec3(const data_type data) : _data(data) {}
+
+    data_type _data;
 };
 
 /** @brief write vector as space separated components */
@@ -122,17 +140,17 @@ inline std::ostream& operator<<(std::ostream& os, const Vec3 v) {
 
 /** @brief add elementwise */
 inline constexpr Vec3 operator+(const Vec3& v1, const Vec3& v2) {
-    return Vec3(v1[0] + v2[0], v1[1] + v2[1], v1[2] + v2[2]);
+    return Vec3(v1._data + v2._data);
 }
 
 /** @brief subtract elementwise */
 inline constexpr Vec3 operator-(const Vec3& v1, const Vec3& v2) {
-    return Vec3(v1[0] - v2[0], v1[1] - v2[1], v1[2] - v2[2]);
+    return Vec3(v1._data - v2._data);
 }
 
 /** @brief multiply elementwise */
 inline constexpr Vec3 operator*(const Vec3& v, const Scalar f) {
-    return Vec3(v[0] * f, v[1] * f, v[2] * f);
+    return Vec3(v._data * f);
 }
 
 /** @brief multiply elementwise */
@@ -142,19 +160,17 @@ inline constexpr Vec3 operator*(const Scalar f, const Vec3& v) {
 
 /** @brief divide elementwise */
 inline constexpr Vec3 operator/(const Vec3& v, const Scalar f) {
-    const Scalar f_inv = 1 / f;
-    return Vec3(v[0] * f_inv, v[1] * f_inv, v[2] * f_inv);
+    return Vec3(v._data / f);
 }
 
 /** @brief scalar product (inner product) */
 inline constexpr Scalar dot(const Vec3& v1, const Vec3& v2) {
-    return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];
+    return glm::dot(v1._data, v2._data);
 }
 
 /** @brief cross product (vector product) */
 inline constexpr Vec3 cross(const Vec3& v1, const Vec3& v2) {
-    return Vec3(v1[1] * v2[2] - v1[2] * v2[1], v1[2] * v2[0] - v1[0] * v2[2],
-                v1[0] * v2[1] - v1[1] * v2[0]);
+    return Vec3(glm::cross(v1._data, v2._data));
 }
 
 /** @brief get normalized vector */
