824a5fe7af0f4aad82f3e62bdda4333570b7cb82
Kwasniok <Kwasniok@users.noreply.github.com>
2022-06-04 21:38:06 +0200

    refactor animator

--- include/cpp_raytracing/entities/entity.hpp
+++ include/cpp_raytracing/entities/entity.hpp
@@ -6,6 +6,8 @@
 #ifndef CPP_RAYTRACING_ENTITIES_ENTITY_HPP
 #define CPP_RAYTRACING_ENTITIES_ENTITY_HPP
 
+#include <memory>
+
 #include "../animator.hpp"
 #include "../hit_record.hpp"
 #include "../identifier.hpp"
@@ -19,11 +21,6 @@ namespace cpp_raytracing {
 struct Entity {
     /** @brief unique entity identifier */
     Identifier<class Entity> id;
-    /**
-     * @brief animator of the entity (optional)
-     * @note `nullptr` means the entity is static.
-     */
-    Animator* animator;
 
     /** @brief default construct with default idenfifier root */
     Entity() = default;
@@ -35,12 +32,22 @@ struct Entity {
     virtual ~Entity() = default;
 
     /**
+     * @brief sets the animator
+     * @note none removes the animator and means the entity is static
+     */
+    virtual void set_animator(std::unique_ptr<Animator>&& animator) {
+        _animator = std::move(animator);
+    }
+    /** @brief returns the animator of the entity */
+    virtual Animator& animator() { return *_animator; }
+
+    /**
      * @brief requests entity to take its appearence for the given time
      * @note A static entity might ignore this entirely.
      */
     virtual void set_time(const Scalar time) {
-        if (animator != nullptr) {
-            animator->update_for_time(time, this);
+        if (_animator) {
+            _animator->update_for_time(time, this);
         }
     }
 
@@ -53,6 +60,13 @@ struct Entity {
                                  const Scalar t_max = infinity) const {
         return {.t = infinity};
     }
+
+  private:
+    /**
+     * @brief animator of the entity (optional)
+     * @note none means the entity is static.
+     */
+    std::unique_ptr<Animator> _animator;
 };
 
 /** @brief default identifier for entitys */
