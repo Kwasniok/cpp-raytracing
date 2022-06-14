baf3ee871ee99e19127e830affca776142f0b3fc
Kwasniok <Kwasniok@users.noreply.github.com>
2022-06-14 13:40:52 +0200

    fix example

--- src/examples/benchmark.cpp
+++ src/examples/benchmark.cpp
@@ -164,7 +164,7 @@ void render_ppm(const RenderConfig& config) {
  * @brief program entry point
  */
 int main(int argc, char** argv) {
-    argparse::ArgumentParser parser("complex");
+    argparse::ArgumentParser parser("benchmark");
     parser.add_argument("-o", "--out").required().help("file output path");
     parser.add_argument("-v", "--verbose")
         .default_value<bool>(false) // store_true
