eb91498a13d11f6e1d74a61ceefb2c4a5a9040e2
Kwasniok <Kwasniok@users.noreply.github.com>
2022-06-16 17:43:16 +0200

    extend math

--- include/cpp_raytracing/math.hpp
+++ include/cpp_raytracing/math.hpp
@@ -12,6 +12,7 @@
 #include <glm/detail/qualifier.hpp>
 #include <glm/geometric.hpp>
 #include <glm/mat3x3.hpp>
+#include <glm/vec2.hpp>
 #include <glm/vec3.hpp>
 
 #include "random.hpp"
@@ -19,8 +20,157 @@
 
 namespace cpp_raytracing {
 
+class Vec2;
 class Vec3;
 class Mat3x3;
+
+/**
+ * @brief 2D floating-point Vector
+ */
+class Vec2 {
+  public:
+    /** @brief initialize as zero vector */
+    constexpr Vec2() : _data{0, 0} {}
+    /** @brief initialize with coefficients */
+    constexpr Vec2(const Scalar u, const Scalar v) : _data{u, v} {}
+
+    /** @brief initialize coefficients with random values */
+    inline static Vec2 random(Scalar min, Scalar max) {
+        return {random_scalar(min, max), random_scalar(min, max)};
+    }
+
+    /** @brief get u coefficient */
+    constexpr Scalar u() const { return _data.x; }
+    /** @brief get v coefficient */
+    constexpr Scalar v() const { return _data.y; }
+
+    /** @brief const iterator for first element */
+    constexpr auto begin() const { return &_data[0]; }
+    /** @brief const iterator for end */
+    constexpr auto end() const { return &_data[0] + 2; }
+    /** @brief iterator for first element */
+    constexpr auto begin() { return &_data[0]; }
+    /** @brief iterator for end */
+    constexpr auto end() { return &_data[0] + 2; }
+
+    /** @brief tests equivalence */
+    constexpr bool operator==(const Vec2& other) const {
+        return _data == other._data;
+    }
+
+    /** @brief tests inequivalence */
+    constexpr bool operator!=(const Vec2& other) const {
+        return _data != other._data;
+    }
+
+    /** @brief negate elementwise */
+    constexpr Vec2 operator-() const { return Vec2{-_data}; }
+
+    /**
+     * @brief enumerated accces to coefficients
+     * @note: `0=u, 1=v`
+     */
+    constexpr Scalar operator[](unsigned long i) const { return _data[i]; }
+    /**
+     * @brief enumerated accces to coefficients
+     * @note: `0=u, 1=v`
+     */
+    constexpr Scalar& operator[](unsigned long i) { return _data[i]; }
+
+    /** @brief add elementwise */
+    constexpr Vec2& operator+=(const Vec2& other) {
+        _data += other._data;
+        return *this;
+    }
+    /** @brief subtract elementwise */
+    constexpr Vec2& operator-=(const Vec2& other) {
+        _data -= other._data;
+        return *this;
+    }
+    /** @brief multiply elementwise */
+    constexpr Vec2& operator*=(const Scalar fac) {
+        _data *= fac;
+        return *this;
+    }
+    /** @brief divide elementwise */
+    constexpr Vec2& operator/=(const Scalar fac) {
+        _data /= fac;
+        return *this;
+    }
+
+    /** @brief get length of the vector */
+    constexpr Scalar length() const {
+        // NOTE: for version 0.9.9 glm::length is not constexpr
+        return sqrt(length_squared());
+    }
+
+    /**
+     * @brief get length of the vector squared
+     * @note Fater than std::pow(length(),2)`
+     */
+    constexpr Scalar length_squared() const { return glm::dot(_data, _data); }
+
+    /** @brief tests if vector is zero vector */
+    constexpr bool near_zero(const Scalar epsilon) const {
+        return std::abs(_data.x) < epsilon && std::abs(_data.y) < epsilon;
+    }
+
+    friend constexpr Vec2 operator+(const Vec2&, const Vec2&);
+    friend constexpr Vec2 operator-(const Vec2&, const Vec2&);
+    friend constexpr Vec2 operator*(const Vec2&, const Scalar);
+    friend constexpr Vec2 operator*(const Scalar, const Vec2&);
+    friend constexpr Vec2 operator/(const Vec2&, const Scalar);
+    friend constexpr Scalar dot(const Vec2&, const Vec2&);
+    friend constexpr Vec2 unit_vector(const Vec2&);
+
+  private:
+    using data_type = glm::vec<2, Scalar>;
+    constexpr Vec2(const data_type& data) : _data(data) {}
+    constexpr Vec2(data_type&& data) : _data(std::move(data)) {}
+
+    data_type _data;
+};
+
+/** @brief write vector as space separated components */
+inline std::ostream& operator<<(std::ostream& os, const Vec2 v) {
+    os << "Vec2(" << v[0] << ", " << v[1] << ")";
+    return os;
+}
+
+/** @brief add elementwise */
+inline constexpr Vec2 operator+(const Vec2& v1, const Vec2& v2) {
+    return Vec2(v1._data + v2._data);
+}
+
+/** @brief subtract elementwise */
+inline constexpr Vec2 operator-(const Vec2& v1, const Vec2& v2) {
+    return Vec2(v1._data - v2._data);
+}
+
+/** @brief multiply elementwise */
+inline constexpr Vec2 operator*(const Vec2& v, const Scalar f) {
+    return Vec2(v._data * f);
+}
+
+/** @brief multiply elementwise */
+inline constexpr Vec2 operator*(const Scalar f, const Vec2& v) {
+    return v * f;
+}
+
+/** @brief divide elementwise */
+inline constexpr Vec2 operator/(const Vec2& v, const Scalar f) {
+    return Vec2(v._data / f);
+}
+
+/** @brief scalar product (inner product) */
+inline constexpr Scalar dot(const Vec2& v1, const Vec2& v2) {
+    return glm::dot(v1._data, v2._data);
+}
+
+/** @brief get normalized vector */
+inline constexpr Vec2 unit_vector(const Vec2& v) {
+    return v / v.length();
+}
 
 /**
  * @brief 3D floating-point Vector
