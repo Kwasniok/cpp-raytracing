fda1af36ca3f95bc2d5e5da9f1b142f99a38c647
Kwasniok <Kwasniok@users.noreply.github.com>
2022-09-15 03:07:26 +0200

    add Jacobi matrix

--- include/cpp_raytracing/geometry/base.hpp
+++ include/cpp_raytracing/geometry/base.hpp
@@ -28,6 +28,22 @@ class Geometry {
     virtual std::unique_ptr<Ray>
     ray_passing_through(const Vec3 start, const Vec3 target) const = 0;
 
+    /**
+     * @brief returns the local Jacobian matrix converting vectors to
+     *         canonical orthonormal representation
+     * @param position curved space cooordinates
+     * @see from_onb_jacobian
+     */
+    virtual Mat3x3 to_onb_jacobian(const Vec3 position) const = 0;
+    /**
+     * @brief returns the local Jacobian matrix converting vectors back from the
+     *        canonical orthonormal representation
+     * @param position curved space cooordinates
+     * @note Is the inverse matrix of `to_onb_jacobian(position)`.
+     * @note to_onb_jacobian
+     */
+    virtual Mat3x3 from_onb_jacobian(const Vec3 position) const = 0;
+
     /** @brief returns the local metric */
     virtual Mat3x3 metric(const Vec3 position) const = 0;
 };
