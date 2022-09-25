973b53cb4782b2076a7addeb6e495ec6c5a349bd
Kwasniok <Kwasniok@users.noreply.github.com>
2022-09-25 16:09:22 +0200

    update tests

--- src/test/cpp_raytracing/algoritm/runge_kutta_4.cpp
+++ src/test/cpp_raytracing/algoritm/runge_kutta_4.cpp
@@ -28,18 +28,19 @@ void test_runge_kutta_4_delta_constant_velocity() {
     const Vec3 x0 = {2, 3, 5};   // position
     const Vec3 v0 = {7, 11, 13}; // velocity
     const Vec6 p0 = {x0, v0};    // phase
+    constexpr Scalar delta_t = 0.17;
 
     // no acceleration
     const std::function<Vec6(const Vec6&)> f = [](const Vec6& p) {
         return Vec6{p.second_half(), Vec3::zero()};
     };
 
-    Vec6 delta_p = runge_kutta_4_delta(f, p0, 1.0);
+    Vec6 delta_p = runge_kutta_4_delta(f, p0, delta_t);
 
     Vec3 delta_x = delta_p.first_half();
     Vec3 delta_v = delta_p.second_half();
 
-    TEST_ASSERT_ALMOST_EQUAL_ITERABLE(delta_x, v0, epsilon);
+    TEST_ASSERT_ALMOST_EQUAL_ITERABLE(delta_x, v0 * delta_t, epsilon);
     TEST_ASSERT_ALMOST_EQUAL_ITERABLE(delta_v, Vec3::zero(), epsilon);
 }
 
@@ -49,21 +50,23 @@ void test_runge_kutta_4_delta_constant_force() {
     const Vec3 v0 = {7, 11, 13}; // velocity
     const Vec3 a = {17, 19, 23}; // acceleration
     const Vec6 p0 = {x0, v0};    // phase
+    constexpr Scalar delta_t = 0.29;
 
     // no acceleration
     const std::function<Vec6(const Vec6&)> f = [a = a](const Vec6& p) {
         return Vec6{p.second_half(), a};
     };
 
-    Vec6 delta_p = runge_kutta_4_delta(f, p0, 1.0);
+    Vec6 delta_p = runge_kutta_4_delta(f, p0, delta_t);
 
     Vec3 delta_x = delta_p.first_half();
     Vec3 delta_v = delta_p.second_half();
 
     // x1 = x0 + v0 * t + 0.5 * a * t ** 2
-    TEST_ASSERT_ALMOST_EQUAL_ITERABLE(delta_x, v0 + 0.5 * a, epsilon);
+    TEST_ASSERT_ALMOST_EQUAL_ITERABLE(
+        delta_x, (v0 + 0.5 * a * delta_t) * delta_t, epsilon);
     // v1 = v0 + a * t
-    TEST_ASSERT_ALMOST_EQUAL_ITERABLE(delta_v, a, epsilon);
+    TEST_ASSERT_ALMOST_EQUAL_ITERABLE(delta_v, a * delta_t, epsilon);
 }
 
 void test_runge_kutta_4_delta_stability() {
